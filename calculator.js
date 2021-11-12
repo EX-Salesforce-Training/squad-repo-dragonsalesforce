705fb5f9c69ebc0c30af340f19ad44320d8b7541
Max Goren <65133477+maxgoren@users.noreply.github.com>
2021-11-12 10:16:12 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -6,30 +6,30 @@ window.onload = function() {
 }
 
 function doAdd() {
-    let num1 = parseInt(document.getElementById("#num1").value)
-    let num2 = parseInt(document.getElementById("#num2").value)
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
     let answer = num1 + num2;
     document.getElementById("ans").value = answer
 }
 
 function doSubtract() {
-    let num1 = parseInt(document.getElementById("#num1").value)
-    let num2 = parseInt(document.getElementById("#num2").value)
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
     let answer = num1 - num2;
     document.getElementById("ans").value = answer
 
 }
 
 function doMultiply() {
-    let num1 = parseInt(document.getElementById("#num1").value)
-    let num2 = parseInt(document.getElementById("#num2").value)
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
     let answer = num1 * num2;
     document.getElementById("ans").value = answer
 }
 
 function doDivide() {
-    let num1 = parseInt(document.getElementById("#num1").value)
-    let num2 = parseInt(document.getElementById("#num2").value)
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
     let answer = num1 / num2;
     document.getElementById("ans").value = answer
 }
