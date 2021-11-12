b5f5ff416a7c5e32e8cdd2b43afce7c012717dac
Max Goren <65133477+maxgoren@users.noreply.github.com>
2021-11-12 10:08:24 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,35 +1,36 @@
-function addition() {
-    let num1 = document.getElementById("#num1").value
-    let num2 = document.getElementById("#num2").value
+window.onload = function() {
+    document.querySelector("addButton").addEventListener("click", doAdd)
+    document.querySelector("subButton").addEventListener("click", doSubtract)
+    document.querySelector("multButton").addEventListener("click", doMultiply)
+    document.querySelector("divButton").addEventListener("click", doDivide)
+}
+
+function doAdd() {
+    let num1 = parseInt(document.getElementById("#num1").value)
+    let num2 = parseInt(document.getElementById("#num2").value)
     let answer = num1 + num2;
     document.getElementById("ans").value = answer
 }
 
-function subtraction() {
-    let num1 = document.getElementById("#num1").value
-    let num2 = document.getElementById("#num2").value
+function doSubtract() {
+    let num1 = parseInt(document.getElementById("#num1").value)
+    let num2 = parseInt(document.getElementById("#num2").value)
     let answer = num1 - num2;
     document.getElementById("ans").value = answer
 
 }
 
-function mult() {
-    let num1 = document.getElementById("#num1").value
-    let num2 = document.getElementById("#num2").value
+function doMultiply() {
+    let num1 = parseInt(document.getElementById("#num1").value)
+    let num2 = parseInt(document.getElementById("#num2").value)
     let answer = num1 * num2;
     document.getElementById("ans").value = answer
 }
 
-function divide() {
-    let num1 = document.getElementById("#num1").value
-    let num2 = document.getElementById("#num2").value
+function doDivide() {
+    let num1 = parseInt(document.getElementById("#num1").value)
+    let num2 = parseInt(document.getElementById("#num2").value)
     let answer = num1 / num2;
     document.getElementById("ans").value = answer
 }
 
-window.onload = function() {
-    document.querySelector("addButton").addEventListener("click", addition)
-    document.querySelector("subButton").addEventListener("click", subtraction)
-    document.querySelector("multButton").addEventListener("click", mult)
-    document.querySelector("divButton").addEventListener("click", divide)
-}
